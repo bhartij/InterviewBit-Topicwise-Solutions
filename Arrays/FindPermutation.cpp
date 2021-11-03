a0babdbc492c7b7b91d520e293dd62e0f8ee5ef4
123_456 <68056115+bharu6522@users.noreply.github.com>
2021-11-03 17:03:47 +0530

    Update FindPermutation.cpp

--- Arrays/FindPermutation.cpp
+++ Arrays/FindPermutation.cpp
@@ -1,50 +1,22 @@
 
 vector<int> Solution::findPerm(const string A, int B) {
-    int n = A.size();
-    int num_I =0,num_D =0;
-    for(int i=0;i<n;i++)
-    {
-        if(A[i] == 'I')
-        {
-            num_I++;
+
+    int n= A.length()+1 ;
+    vector<int> ans ;
+    int end = B  ;
+    int start = 1 , i=0 ;
+    while(i< n){
+        if(A[i] == 'D'){
+            ans.push_back(end) ;
+            end--; 
         }
-        else
-        {
-            num_D++;
+        else{
+            ans.push_back(start) ;
+            start++ ;
         }
-    }
-    vector<int> toreturn;
-    int ind_I,ind_D;
-    if(A[0] == 'I')
-    {
-        ind_I = (n+1)-num_I;
-        ind_D = ind_I-1;
-        toreturn.push_back(ind_I);
-        ind_I++;
-    }
-    else
-    {
-        ind_I = (B+1)-num_I;
-        ind_D = ind_I-1;
-        toreturn.push_back(ind_D);
-        ind_D--;
+        i++ ;
     }
     
+    return ans ;
     
-    for(int i=0;i<n;i++)
-    {
-        if(A[i] == 'I')
-        {
-            toreturn.push_back(ind_I);
-            ind_I++;
-        }
-        else
-        {
-            toreturn.push_back(ind_D);
-            ind_D--;
-        }
-        
-    }
-    
-    return toreturn;
 }
